63f162eec7ee97743003a188b834ce5443ef0f1e
AshakaE <ashakaegerega@gmail.com>
2021-04-28 12:07:16 +0100

    Add babel config

--- webpack/base.js
+++ webpack/base.js
@@ -1,52 +1,52 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        include: path.resolve(__dirname, "src/"),
+        include: path.resolve(__dirname, 'src/'),
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["env"],
+            presets: ['env'],
           },
         },
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader",
+        use: 'raw-loader',
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml|mp3)$/i,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../"),
+      root: path.resolve(__dirname, '../'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
   ],
 };
